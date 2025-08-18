fafaaa62e1b0aba4f9eee15817d5e710dffecc2c
shinespark <shnsprk@gmail.com>
2025-08-19 01:05:28 +0900

    1-5 Use SwiftUI views from other frameworks

---
+++ Landmarks3/MapView.swift
@@ -0,0 +1,29 @@
+//
+//  MapView.swift
+//  Landmarks3
+//
+//  Created by shinespark on 2025/08/19.
+//
+
+import MapKit
+import SwiftUI
+
+struct MapView: View {
+    var body: some View {
+        Map(initialPosition: .region(region))
+    }
+
+    private var region: MKCoordinateRegion {
+        MKCoordinateRegion(
+            center: CLLocationCoordinate2D(
+                latitude: 34.011_286,
+                longitude: -116.166_868
+            ),
+            span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
+        )
+    }
+}
+
+#Preview {
+    MapView()
+}
