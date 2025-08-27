11a4b28165f6d5bea6bd2e0cb592c6e01bfbfe83
shinespark <shnsprk@gmail.com>
2025-08-28 02:27:57 +0900

    2-1-1 Create drawing data for a badge view

---
+++ Landmarks3/Views/BadgeBackground.swift
@@ -0,0 +1,18 @@
+//
+//  BadgeBackground.swift
+//  Landmarks3
+//
+//  Created by shinespark on 2025/08/28.
+//
+
+import SwiftUI
+
+struct BadgeBackground: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BadgeBackground()
+}
