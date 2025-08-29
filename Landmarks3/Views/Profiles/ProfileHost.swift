cf984c328d71af8639d2a40d587ca82ab9cb9853
shinespark <shinespark@users.noreply.github.com>
2025-08-30 01:00:18 +0900

    3-2-1 Display a user profile

---
+++ Landmarks3/Views/Profiles/ProfileHost.swift
@@ -0,0 +1,23 @@
+//
+//  ProfileHost.swift
+//  Landmarks3
+//
+//  Created by shinespark on 2025/08/30.
+//
+
+import SwiftUI
+
+struct ProfileHost: View {
+    @State private var draftProfile = Profile.default
+
+    var body: some View {
+        VStack(alignment: .leading, spacing: 20) {
+            ProfileSummary(profile: draftProfile)
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ProfileHost()
+}
